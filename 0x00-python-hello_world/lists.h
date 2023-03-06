4ccfdbcdc551ed529a1b57e8a87462b9d32f529f
Amr Azouz <amrsalahazouz@gmail.com>
2023-03-07 00:30:06 +0200

    #

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,15 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdlib.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * for ALX project
  */
 typedef struct listint_s
 {
